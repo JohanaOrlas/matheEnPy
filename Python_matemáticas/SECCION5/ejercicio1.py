49c77a8529d15188f0ee032d955d6d6b552704eb
ValentinaOrlas <johanavop25@gmail.com>
2023-11-27 22:19:46 -0500

    Sección 5 terminada

--- Python_matemáticas/SECCION5/ejercicio1.py
+++ Python_matemáticas/SECCION5/ejercicio1.py
@@ -8,6 +8,7 @@ for i in range(0,101):
     print(i)
 
 print("Ahora los números del 0 al 100 de forma descendente")
+#Ciclo que imprime los números de 0 a 100 de forma descendente
 for i in range(101,0,-1):
     print(i)
 

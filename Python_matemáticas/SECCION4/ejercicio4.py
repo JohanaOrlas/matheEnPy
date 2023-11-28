771f1d4b9f6e6eccbc17c0efaa69450af01ab5f9
ValentinaOrlas <johanavop25@gmail.com>
2023-11-27 22:16:13 -0500

    Comentarios agregados

--- Python_matemáticas/SECCION4/ejercicio4.py
+++ Python_matemáticas/SECCION4/ejercicio4.py
@@ -2,3 +2,15 @@
 Autor: Valentina Orlas
 Fecha: 2023
 """
+
+# Declaración de variables a utilizar
+
+numero = int(input("Ingrese un número entre 1 y 15: "))
+
+# Condicional para saber si el número es primo o no 
+
+if numero == 2 or numero == 3 or numero == 5 or numero == 7 or numero == 11 or numero == 13 :
+
+    print("El número es primo")
+else:
+    print("El número no es primo")

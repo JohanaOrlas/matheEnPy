be858e5451cb41bdbc8ed8ef21f6682253b46399
ValentinaOrlas <johanavop25@gmail.com>
2023-11-27 22:15:20 -0500

    Comentarios agregados

--- Python_matemáticas/SECCION1/ejercicio3.py
+++ Python_matemáticas/SECCION1/ejercicio3.py
@@ -5,10 +5,10 @@ Fecha: 20/10/2023
 # Definición de las variables que se utilizarán 
 
 
-montoPrestamo = int(input("Ingrese el monto del préstamo: "))
+montoPrestamo = float(input("Ingrese el monto del préstamo: "))
 plazoMeses = int(input("Ingrese el plazo en meses: "))
 
 #Porceso para calcular la cuota mensual 
-cuotaMensual ="{:3f}".format((montoPrestamo * 0.027) / ( 1 - ( 1 + 0.027 ) ** (-plazoMeses)))#"{:.nf}".format() --> así se muestra n cantidad de números después del punto decimal
+cuotaMensual ="{:.2f}".format((montoPrestamo * 0.027) / ( 1 - ( 1 + 0.027 ) ** (-plazoMeses)))#"{:.nf}".format() --> así se muestra n cantidad de números después del punto decimal
 
 print("La cuota mensual a pagar es de: "+ str(cuotaMensual))
